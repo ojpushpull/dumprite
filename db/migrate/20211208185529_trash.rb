983604d0342f713adf655808fe7fdcc8fcacc42c
Okera Johnson <okerajohnson@gmail.com>
2021-12-08 14:31:41 -0500

    got a table madee

--- db/migrate/20211208185529_trash.rb
+++ db/migrate/20211208185529_trash.rb
@@ -1,4 +1,10 @@
 class Trash < ActiveRecord::Migration[6.1]
   def change
+    create Table :trashs do |t|
+      add_column :trashs, :plastic, :boolean, default: false
+      add_column :trashs, :metal, :boolean, default: false
+      add_column :trashs, :paper, :boolean, default: false
+      t.string :trash_name
+    end
   end
 end
